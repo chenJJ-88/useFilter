5eef742c60e717f49edb12f65261a1f285b17b61
chenJJ-88 <118970817+chenJJ-88@users.noreply.github.com>
2025-02-19 17:33:39 +0800

    修改 type 文件

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,2 @@
-export { useFilter } from './hooks/useFilter';
 export { default as FilterForm } from './components/FilterForm';
-export type { UseFilterOptions, UseFilterReturn } from './types'; 
+export type { UseFilterOptions, UseFilterReturn, IFilters } from './types'; 
